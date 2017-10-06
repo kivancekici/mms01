efff04145111837973b9a8eb378d591ce5d01328
Askin SAGLAM <askin.saglam@normcvt.local>
2017-10-06 17:27:20 +0300

    deneme

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -48,11 +48,12 @@ function checkLogin() {
 
             // mainView.showNavbar(false);
             // mainView.router.loadPage({ url: 'main.html', ignoreCache: true });
+            /*
             mainView.router.load({
                 template: Template7.templates.mainTemplate // template already compiled and available as a property of Template7.templates
             });
 
-
+            */
 
         } else {
 
