1167cfa76d5a1f7d88fa6f4984df840d170506d8
askinsaglam <askin.saglam@normcvt.local>
2017-12-05 17:04:13 +0300

    sadasd

--- www/js/templateHelper.js
+++ www/js/templateHelper.js
@@ -4,6 +4,22 @@ function initListVirtualManufacturers() {
     listVirtualManufacturers = myApp.virtualList('.lstmanufacturers', {
         items: [
         ],
+        searchAll: function (query, items) {
+            var foundItems = [];
+            for (var i = 0; i < items.length; i++) {
+                // Check if title contains query string
+                var found=false;
+
+                if (items[i].name.toLowerCase().indexOf(query.trim()) >= 0) found=true;
+               //Locationda gelebilir.
+               // if (items[i].description_short.toLowerCase().indexOf(query.trim()) >= 0) found=true;
+               // if (items[i].description.toLowerCase().indexOf(query.trim()) >= 0) found=true;
+                
+                if(found) foundItems.push(i);;
+            }
+            // Return array with indexes of matched items
+            return foundItems; 
+        },
         height: 61,
         template: '<li>' +
         '<a href="#" onclick="showManufacturerMenu('+"'{{name}}'"+',{{id_manufacturer}});" class="item-link item-content">' +
