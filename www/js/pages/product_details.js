af8be4f5e312b6004ca91240910a2bae906143cc
kivancekici <kivancekici@gmail.com>
2018-01-14 18:19:55 +0300

    rtrt

--- www/js/pages/product_details.js
+++ www/js/pages/product_details.js
@@ -1,3 +1,6 @@
 function initPageProductDetails(){
+    $$(".product_name_title").text(currentProduct.productname);
     
+    $$(".product_name_over_image").text(currentProduct.manufacname);
+    $$('.card-header-product').css('background-image', 'url("'+currentProduct.imgdirectory+'")');
 }
