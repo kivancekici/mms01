bdf872b92566df1ce681703ea516bb7b4edfbb57
kivancekici <kivancekici@gmail.com>
2018-01-14 18:34:07 +0300

    cfdfgdfg

--- platforms/browser/www/js/pages/product_details.js
+++ platforms/browser/www/js/pages/product_details.js
@@ -1,6 +1,9 @@
 function initPageProductDetails(){
     $$(".product_name_title").text(currentProduct.productname);
     
-    $$(".product_name_over_image").text(currentProduct.manufacname);
-    $$('.card-header-product').css('background-image', 'url("'+currentProduct.imgdirectory+'")');
+    $$(".product_name_over_image").text(currentProduct.productname);
+    $$(".product-manufacturer-name").text(currentProduct.manufacname);
+    $$(".product-description").html(currentProduct.description);
+    
+    $$('.card-header-product').css('background-image', 'url("http://baklava7.de'+currentProduct.imgdirectory+'")');
 }
--- www/js/product.js
+++ www/js/product.js
@@ -19,42 +19,4 @@ function showProductDetailsModal(idProduct) {
 
     return false;
 
-    myApp.modal({
-        title: '<div class="buttons-row">' +
-            '<a href="#tab1" class="button active tab-link">Tab 1</a>' +
-            '<a href="#tab2" class="button tab-link">Tab 2</a>' +
-            '<a href="#tab3" class="button tab-link">Tab 3</a>' +
-            '</div>',
-        text: '<div class="tabs">' +
-            '<div class="tab active" id="tab1">' +
-            '<p>' + currentProduct.manufacname + '</p>' +
-            '<p>' + currentProduct.productname + '</p>' +
-            '<p>' + currentProduct.description + '</p>' +
-            '<p>' + currentProduct.imgdirectory + '</p>' +
-            '</div>' +
-            '<div class="tab" id="tab2">' +
-            '<p> Gross:' + currentProduct.prices.grossprice + '</p>' +
-            '<p> Reduced:' + currentProduct.prices.reducedprice + '</p>' +
-            '</div>' +
-
-            '<div class="tab" id="tab3">' +
-            '<p> Gross:' + currentProduct.unit.name + '</p>' +
-            '<p> Gross:' + currentProduct.unit.name + '</p>' +
-            '</div>' +
-            '</div>',
-        buttons: [
-            {
-                text: 'Sepete Ekle',
-                bold: true,
-                onClick: function () {
-                    myApp.alert('Sepete eklenecek!')
-                },
-            },
-            {
-                text: 'Kapat',
-                bold: true
-            },
-        ]
-    });
-
 }
